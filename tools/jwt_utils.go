36a82df8b63e3634bec303b4775212812530c89a
Tataro <tat.kittitat@gmail.com>
2024-08-11 21:52:47 +0700

    add jwt util

---
+++ tools/jwt_utils.go
@@ -0,0 +1,54 @@
+package tools
+
+import (
+	"errors"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/tat-101/bb-assignment-back/config"
+)
+
+type Claims struct {
+	Email string `json:"email"`
+	jwt.RegisteredClaims
+}
+
+// GenerateJWT generates a JWT token for a given email.
+func GenerateJWT(email string) (string, error) {
+	expirationTime := time.Now().Add(24 * time.Hour)
+	claims := &Claims{
+		Email: email,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(expirationTime),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(config.GetJWTSecret()))
+}
+
+// ValidateJWT validates a given JWT token and returns the claims if valid.
+func ValidateJWT(tokenString string) (*Claims, error) {
+	claims := &Claims{}
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte(config.GetJWTSecret()), nil
+	})
+
+	if err != nil {
+		if errors.Is(err, jwt.ErrTokenMalformed) {
+			return nil, errors.New("token is malformed")
+		} else if errors.Is(err, jwt.ErrTokenSignatureInvalid) {
+			return nil, errors.New("invalid token signature")
+		} else if errors.Is(err, jwt.ErrTokenExpired) {
+			return nil, errors.New("token is expired")
+		} else {
+			return nil, errors.New("token is invalid")
+		}
+	}
+
+	if !token.Valid {
+		return nil, errors.New("token is not valid")
+	}
+
+	return claims, nil
+}
